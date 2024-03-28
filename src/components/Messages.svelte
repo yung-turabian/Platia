18a6aa405297628f59e7e863c3fa56f84ecf6d02
Henry <77937076+yung-turabian@users.noreply.github.com>
2024-03-28 10:08:34 -0400

    Update Messages.svelte

--- src/components/Messages.svelte
+++ src/components/Messages.svelte
@@ -34,7 +34,26 @@
   onDestroy(() => {
     unsubscribe?.();
   });
+
+import { currentUser, pb } from '../lib/pocketbase';
+import { user } from 'Messages.svelte';
+
+    let newMessage: string;
+
+  async function sendMessage() {
+    const data = {
+      message: newMessage,
+      user: $currentUser.id,
+    };
+    const createdMessage = await pb.collection('chatroom1').create(data);
+    newMessage = '';
+  }
 </script>
+
+<form on:submit|preventDefault={sendMessage}>
+    <input placeholder="Message" type="text" bind:value={newMessage} />
+    <button type="submit">Send</button>
+</form>
 
 <div class="messages">
   {#each messages as message (message.id)}
